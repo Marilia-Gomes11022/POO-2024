e73f3efefb00582654989713ac438b4c99736a79
Marilia-Gomes11022 <160539701+Marilia-Gomes11022@users.noreply.github.com>
2024-11-05 14:04:40 -0300

    Add files via upload

---
+++ file01.py
@@ -0,0 +1,16 @@
+x = int(input('Digite um valor: '))
+if x > 10:
+    print('O número digitado é maior que 10')
+else: 
+    print('O número digitado é menor que 10')
+
+
+
+x = float(input('Digite a média final do aluno: '))
+if x >= 6:
+    print('O aluno está APROVADO')
+elif 3 < x < 6:
+    print('O aluno está em RECUPERAÇÃO')
+else:
+    print('O aluno está REPROVADO')
+    
